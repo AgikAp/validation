68c69394bcdbb107da9ef2a577f6e9becc6d88b6
agik.ap <agik.ap@bts.id>
2024-01-13 20:14:12 +0700

    add validation not empty string

--- messages.go
+++ messages.go
@@ -1,12 +1,14 @@
 package validations
 
 var constantMessage map[string]string = map[string]string{
-	"max":   "max value is %v characters",
-	"min":   "min value is %v characters",
-	"email": "invalid format email",
-	"image": "file is not image",
-	"gt":    "numbers cannot greater than %v",
-	"lt":    "numbers cannot less than %v",
+	"max":      "max value is %v characters",
+	"min":      "min value is %v characters",
+	"email":    "invalid format email",
+	"image":    "file is not image",
+	"gt":       "numbers cannot greater than %v",
+	"lt":       "numbers cannot less than %v",
+	"notEmpty": "field %v should not emtpy",
+	"regx":     "not valid regx %v",
 }
 
 var errorValidations = "error validation field"
